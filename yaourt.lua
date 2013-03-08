123dbc17998cfa3bf33e5684953dd507dd946499
Noah Tilton <noaht@wolfram.com>
2013-03-08 16:50:47 -0600

    yaourt widget (pacman+aur)

---
+++ yaourt.lua
@@ -0,0 +1,38 @@
+---------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2013, Noah K. Tilton <noahktilton@gmail.com>
+---------------------------------------------------
+
+-- {{{ Grab environment
+local io = { popen = io.popen }
+local setmetatable = setmetatable
+-- }}}
+
+module("gnarly.yaourt")
+
+-- Pkg: provides number of pending updates on UNIX systems
+local yaourt = {}
+
+
+-- {{{ Packages widget type
+local function worker()
+    local pacf = io.popen("pacman -Qu")
+    local aurf = io.popen("yaourt --aur -Qu|grep ^aur")
+    pacman = 0
+    aur = 0
+    for _ in pacf:lines() do
+      pacman = pacman + 1
+    end
+    for _ in aurf:lines() do
+      aur = aur + 1
+    end
+    pacf:close()
+    aurf:close()
+    return {
+      ["pacman"] = pacman,
+      ["aur"] = aur,
+    }
+end
+-- }}}
+
+return setmetatable(yaourt, { __call = function(_, ...) return worker(...) end })
