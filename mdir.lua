d26a41f76e068b95a369585e58d09a6b11675dd1
Noah K. Tilton <noah@tilton.co>
2014-02-15 03:17:49 -0600

    don't log if missing dirs

--- mdir.lua
+++ mdir.lua
@@ -24,7 +24,7 @@ local function worker(format, warg)
     if not warg then return end
 
     local mailboxes = {}
-    local f         = io.popen("ls " .. warg .. "/*/new/*|grep -v Junk\\/new")
+    local f         = io.popen("ls " .. warg .. "/*/new/* 2>/dev/null |grep -v Junk\\/new")
 
     if f then
       for line in f:lines() do
