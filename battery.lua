89ec8762124ca0c9aff9747e88b346a255aec73d
Noah Tilton <noah@tilton.co>
2014-09-21 00:30:09 -0500

    blah

--- battery.lua
+++ battery.lua
@@ -45,8 +45,12 @@ end
 -- TODO put this in UTIL
 local function syscall(cmd)
     local f      = popen(cmd)
-    local rs     = f:read("*all")
-    f:close()
+    local rs     = '';
+    if not f == nil then
+            rs     = f:read("*all")
+            f:close()
+    end
+
     return rs
 end
 
